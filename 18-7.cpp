f3d7ece7ef3098cd52a630efdff4d9d7e092f864
RodATS <77297145+RodATS@users.noreply.github.com>
2021-04-14 16:17:40 -0500

    Update 18-7.cpp

--- 18-7.cpp
+++ 18-7.cpp
@@ -2,59 +2,67 @@
 #include <string>
 using namespace std;
 
-class Rot
-{
-  private:
-  int clave=13;
-  string alfabeto=("abcdefghigjlmnopqrstuvwxyz");
 
-  public:
-  Rot();
-  string cifrado(string mensaje)
+ string cifrado(string mensaje)
   {
+    int clave=13;
+    string alfabeto=("abcdefghigjlmnopqrstuvwxyz");
     string mensaje_cifrado;
     int aux=0;
     int indice;
     while(aux < mensaje.length())
+    {
+      if(mensaje[aux]==' ')
+        mensaje_cifrado+=mensaje[aux];
+      else 
+      {
       indice= alfabeto.find(mensaje[aux]);
       if ((indice + clave)>alfabeto.length())
       {
-        int r=alfabeto.length()%(indice + clave);
-        mensaje_cifrado+= alfabeto[r];
+        mensaje_cifrado+= alfabeto[(indice+clave)-alfabeto.length()];
       }
       else
         mensaje_cifrado+= alfabeto[indice+clave];
+      }
       aux++;
+    }
       return mensaje_cifrado;
   }
 
-  string descifrado(string mensaje)
+string descifrado(string mensaje)
   {
+    int clave=13;
+    string alfabeto=("abcdefghigjlmnopqrstuvwxyz");
     string mensaje_descifrado;
     int aux=0;
     int indice;
     while(aux < mensaje.length())
+    {
+    if(mensaje[aux]==' ')
+        mensaje_descifrado+=mensaje[aux];
+      else 
+      {
       indice= alfabeto.find(mensaje[aux]);
       if ((indice - clave)<0)
       {
-        mensaje_descifrado+= alfabeto[(indice+clave)-alfabeto.length()];
+        mensaje_descifrado+= alfabeto[alfabeto.length()-(clave-indice)];
       }
       else
         mensaje_descifrado+= alfabeto[indice-clave];
+      }
       aux++;
+    }
       return mensaje_descifrado;
-  }
+ }
 
-};
 
 int main() {
 
   string message;
   getline(cin, message );
-  Rot Emisor;
-  string mensaje_c= Emisor.cifrado(message);
+  string mensaje_c= cifrado(message);
   cout<<mensaje_c<<endl;
-  string mensaje_d= Emisor.descifrado(mensaje_c);
+  string mensaje_d= descifrado(mensaje_c);
   cout<<mensaje_d<<endl;
 
   return 0;

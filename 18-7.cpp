23b11146275b4c922eb5a85faafe3283af9dfdc9
RodATS <77297145+RodATS@users.noreply.github.com>
2021-04-20 23:07:22 -0500

    Update 18-7.cpp

--- 18-7.cpp
+++ 18-7.cpp
@@ -3,68 +3,96 @@
 using namespace std;
 
 
- string cifrado(string mensaje)
-  {
-    int clave=13;
-    string alfabeto=("abcdefghigjlmnopqrstuvwxyz");
-    string mensaje_cifrado;
-    int aux=0;
-    int indice;
-    while(aux < mensaje.length())
-    {
-      if(mensaje[aux]==' ')
-        mensaje_cifrado+=mensaje[aux];
-      else 
-      {
-      indice= alfabeto.find(mensaje[aux]);
-      if ((indice + clave)>alfabeto.length())
-      {
-        mensaje_cifrado+= alfabeto[(indice+clave)-alfabeto.length()];
-      }
-      else
-        mensaje_cifrado+= alfabeto[indice+clave];
-      }
-      aux++;
-    }
-      return mensaje_cifrado;
-  }
+class Cesar{
+  private:
+  string alfabeto=("abcdefghijklmnopqrstuvwxyz");
+  int clave;
 
-string descifrado(string mensaje)
-  {
-    int clave=13;
-    string alfabeto=("abcdefghigjlmnopqrstuvwxyz");
-    string mensaje_descifrado;
-    int aux=0;
-    int indice;
-    while(aux < mensaje.length())
+  public:
+
+    Cesar(int c){
+      clave=c;}
+      
+  int funcion_modulo(int a, int n)
     {
-    if(mensaje[aux]==' ')
-        mensaje_descifrado+=mensaje[aux];
-      else 
+      int q,r;
+      q=a/n;//hallamos el cociente
+      r=a%n;//hallamos el residuo
+      while(r<0)//mientras que el residuo sea negativo
       {
-      indice= alfabeto.find(mensaje[aux]);
-      if ((indice - clave)<0)
-      {
-        mensaje_descifrado+= alfabeto[alfabeto.length()-(clave-indice)];
+        q--;//el cociente hallado se reducirá en 1 hasta que el residuo sea positivo
+        r=a-(n*q);
       }
-      else
-        mensaje_descifrado+= alfabeto[indice-clave];
-      }
-      aux++;
+      return r;
     }
-      return mensaje_descifrado;
+   //////////////////////////////////////////////////////// 
+  string cifrado_cesar(string mensaje)
+    {      
+      string mensaje_cifrado;
+      int aux=0;
+      int indice;
+      while(aux < mensaje.length())
+      {
+        if(alfabeto.find(mensaje[aux])<alfabeto.length())//si no existe en el alfabeto
+        {
+        indice= alfabeto.find(mensaje[aux]);
+        if(clave<=alfabeto.length()){//por si la clave es menor a la longitud del alfabeto usamos modulo
+          if ((indice + clave)>alfabeto.length())
+          {
+            int aux=funcion_modulo((indice+clave),alfabeto.length());
+            if(aux==0)
+              mensaje_cifrado+= alfabeto[indice];//cuando la clave sea 26-longitud del alfabeto
+            else
+              mensaje_cifrado+= alfabeto[aux];
+          }
+          else
+            mensaje_cifrado+= alfabeto[indice+clave];
+          }
+        
+        else{
+          if(indice+clave>alfabeto.length()*2)//en caso que la clave supere el doble de la longitud del alfabeto
+          mensaje_cifrado+= alfabeto[(clave-indice)-(alfabeto.length()-indice)];
+
+          else mensaje_cifrado+= alfabeto[(indice+clave)-alfabeto.length()];
+        }
+      }
+        aux++;
+      }
+        return mensaje_cifrado;
+    }
+////////////////////////////////////////////////////////////
+  string descifrado_cesar(string mensaje)
+    {
+      string mensaje_descifrado;
+      int aux=0;
+      int indice;
+      while(aux < mensaje.length())
+      {
+      if(alfabeto.find(mensaje[aux])<alfabeto.length())
+        {
+        indice= alfabeto.find(mensaje[aux]);
+        if ((indice - clave)<0)
+        {
+          mensaje_descifrado+= alfabeto[alfabeto.length()-(clave-indice)];
+        }
+        else
+          mensaje_descifrado+= alfabeto[indice-clave];
+        }
+        aux++;
+      }
+        return mensaje_descifrado;
  }
-
+};
 
 int main() {
 
   string message;
   getline(cin, message );
-  string mensaje_c= cifrado(message);
+  Cesar Em(20); //max num 26 clave pero no se recomienda
+  string mensaje_c= Em.cifrado_cesar(message);
   cout<<mensaje_c<<endl;
-  string mensaje_d= descifrado(mensaje_c);
+  string mensaje_d= Em.descifrado_cesar(mensaje_c);
   cout<<mensaje_d<<endl;
-
   return 0;
 
 }

6d434b5bd2f511bd6b542b91ccb3c86668cc036a
RodATS <77297145+RodATS@users.noreply.github.com>
2021-04-22 11:28:29 -0500

    Update cesar.cpp

--- cesar.cpp
+++ cesar.cpp
@@ -78,6 +78,12 @@ class Cesar{
  }
 };
 
+
+void vulnerar(string);
+
+void vulnerar_fuerza_bruta(string);
+
+
 int main() {
   cout<<"ingresa el mensaje:"<<endl;
   string message;
@@ -85,8 +91,58 @@ int main() {
   Cesar Em(5);
   string mensaje_c= Em.cifrado_cesar(message);
   cout<<mensaje_c<<endl;
+  /*
   string mensaje_d= Em.descifrado_cesar(mensaje_c);
   cout<<mensaje_d<<endl;
+*/
+  vulnerar_fuerza_bruta(mensaje_c);
   return 0;
 
+}
+
+
+ int funcion_modulo(int a, int n)
+    {
+      int q,r;
+      q=a/n;//hallamos el cociente
+      r=a%n;//hallamos el residuo
+      while(r<0)//mientras que el residuo sea negativo
+      {
+        q--;//el cociente hallado se reducirá en 1 hasta que el residuo sea positivo
+        r=a-(n*q);
+      }
+      return r;
+    }
+
+
+void vulnerar_fuerza_bruta(string texto_cifrado){
+  int clave=0;
+  string mensaje_descifrado[26];
+  string alfabeto=("abcdefghijklmnopqrstuvwxyz");
+  while(clave<26)
+  {
+    string mensaje;
+    int aux=0;
+    while(aux<texto_cifrado.length())
+    {
+      if(alfabeto.find(texto_cifrado[aux])<alfabeto.length())
+        {
+          int indice= alfabeto.find(texto_cifrado[aux]);   
+          int pos=funcion_modulo((indice+(alfabeto.length()-clave)),alfabeto.length());
+          mensaje+= alfabeto[pos];
+          
+        }
+      aux++;
+    }
+    mensaje_descifrado[clave]=mensaje;
+    clave++;
+  }
+  bool continuar=1;
+  int i=0;
+  while(continuar==1)
+  {
+    cout<<"con clave: "<<i<<": "<<mensaje_descifrado[i]<<endl;
+    cout<<"continuar 0(NO) 1(SI): ";cin>>continuar;cout<<endl;
+    i++;
+  }
 }

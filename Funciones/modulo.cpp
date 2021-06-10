bac6cce73f3364e91d2b6c9fbb5b0242dba2e431
RodATS <77297145+RodATS@users.noreply.github.com>
2021-06-09 19:19:55 -0500

    Update modulo.cpp

--- Funciones/modulo.cpp
+++ Funciones/modulo.cpp
@@ -5,7 +5,7 @@ int funcion_modulo(int a, int n)
 {
   int q,r;
   q=a/n;//hallamos el cociente
-  r=a%n;//hallamos el residuo
+  r=a-(n*q);//hallamos el residuo
   while(r<0)//mientras que el residuo sea negativo
   {
     q--;//el cociente hallado se reducirá en 1 hasta que el residuo sea positivo
